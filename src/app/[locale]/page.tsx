e4b3b9e6b51b59f5eb78223f72062110597cd85c
vinkonhe <vinkonhe@gmail.com>
2024-12-27 20:02:20 +0800

    change color

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -36,13 +36,18 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-full bg-[#F5F4EF] pt-[48px] justify-center">
+    <div className="flex h-full selection:bg-black selection:text-white bg-[#F5F4EF] pt-[48px] justify-center">
       <div className="flex m-4 flex-col max-w-3xl bg-[#F5F4EF] item-center">
         <a href={'/'} target="_parent">
           <Image src={"../bm-logo.svg"} alt={"logo"} width={53} height={26}></Image>
         </a>
         <a className="cursor-default pt-[48px] text-[16px] font-normal text-[#1E1E1D]"><span className="font-semibold">{t('title')}</span>{t('about')}</a>
         <a className="cursor-default pt-[12px] text-[16px] font-normal text-[#1E1E1D]">{t('about-2')}</a>
+        {/* <div className="flex sm:flex-row flex-col border-solid border-[#F6F6F6]">
+          <div className="cursor-pointer border w-full h-[81px] hover:bg-[#ECECEC] border-[#000] bg-[#F5F4EF] mt-[24px] sm:mr-[6px]"></div>
+          <div className="cursor-pointer border w-full h-[81px] hover:bg-[#ECECEC] border-[#000] bg-[#F5F4EF] sm:mt-[24px] mt-[12px] sm:ml-[6px]"></div>
+        </div> */}
+
         <div className="bg-black w-full h-[1px] mt-[32px] mb-[12px]" />
         <div className="flex flex-row justify-end">
 
