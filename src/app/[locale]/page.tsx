290b16e5b758ec1ddfad7ab6bfb5517502ae82de
vinkonhe <vinkonhe@gmail.com>
2024-12-28 18:00:06 +0800

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { useTranslations } from 'next-intl';
 import { useState } from "react";
 import Modal from "../Components/Modal";
 import { useRouter, useParams } from 'next/navigation';
+import Waitlist from "../Components/Waitlist";
 
 
 export default function Home() {
@@ -41,27 +42,32 @@ export default function Home() {
         <a href={'/'} target="_parent">
           <Image src={"../bm-logo.svg"} alt={"logo"} width={53} height={26}></Image>
         </a>
-        <a className="cursor-default pt-[48px] text-[16px] font-normal text-[#1E1E1D]"><span className="font-semibold">{t('title')}</span>{t('about')}</a>
-        <a className="cursor-default pt-[12px] text-[16px] font-normal text-[#1E1E1D]">{t('about-2')}</a>
-        {/* <div className="flex sm:flex-row flex-col border-solid border-[#F6F6F6]">
-          <div className="cursor-pointer border w-full h-[81px] hover:bg-[#ECECEC] border-[#000] bg-[#F5F4EF] mt-[24px] sm:mr-[6px]"></div>
-          <div className="cursor-pointer border w-full h-[81px] hover:bg-[#ECECEC] border-[#000] bg-[#F5F4EF] sm:mt-[24px] mt-[12px] sm:ml-[6px]"></div>
-        </div> */}
+        <a className="cursor-default pt-[48px] text-[16px] font-normal text-[#1E1E1D]">
+          <span className="font-semibold">
+            {t('title')}
+          </span>
+          {t('about')}
+        </a>
+
+
+        <a className="cursor-default pt-[12px] text-[16px] font-normal text-[#1E1E1D]">
+          {t('about-2')}
+        </a>
+
+        <Waitlist></Waitlist>
+
 
         <div className="bg-black w-full h-[1px] mt-[32px] mb-[12px]" />
         <div className="flex flex-row justify-end">
 
-          <button className="m-2 opacity-85 hover:opacity-100 font-normal underline hover:decoration-2" onClick={() => router.replace(`/${params.locale}/blog`)}>
+          <a href={`/${params.locale}/blog`} className="m-2 opacity-85 hover:opacity-100 font-normal underline hover:decoration-2">
             {t2('BlogText')}
-          </button>
+          </a>
 
           <button className="m-2 opacity-85 hover:opacity-100 font-normal underline hover:decoration-2" onClick={() => setIsLangModalOpen(true)}>
             {t2('ModalTitle')}
           </button>
 
-          {/* <button className="m-2 opacity-85 hover:opacity-100 font-normal underline hover:decoration-2" onClick={() => setIsTextModalOpen(true)}>
-            {t('about-text')}
-          </button> */}
         </div>
 
 
