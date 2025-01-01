b2f833fed102af8a27b59041973ff61a816ee56e
vinkonhe <vinkonhe@gmail.com>
2025-01-01 22:07:43 +0800

    update:Blog

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,34 +1,40 @@
-"use client"
+"use client";
 import Image from "next/image";
-import { useRouter } from '@/i18n/routing';
-import { useTranslations } from 'next-intl';
+import { useRouter } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
 import Posts from "@/app/Components/Posts";
 
 export default function Blog() {
-
   const router = useRouter();
-  const t = useTranslations('HomePage');
+  const t = useTranslations("HomePage");
 
   const handleLogoClick = (e: React.MouseEvent) => {
     e.preventDefault();
-    router.push('/');
-    
+    router.push("/");
   };
 
   return (
-    <div className="flex h-full bg-[#F5F4EF] pt-[48px] justify-center">
+    <div className="flex h-full selection:bg-black selection:text-white bg-[#F5F4EF] pt-[48px] justify-center">
       <div className="flex m-4 flex-col max-w-3xl w-full bg-[#F5F4EF] item-center">
-        <a href={'/'} target="_top">
-          <Image src={"../bm-logo.svg"} alt={"logo"} width={53} height={26}></Image>
+        <a href={"/"} target="_top">
+          <Image
+            src={"../bm-logo.svg"}
+            alt={"logo"}
+            width={53}
+            height={26}
+          ></Image>
         </a>
-        <a className="cursor-default pt-[48px] text-[16px] font-normal text-[#1E1E1D] ">{t('BlogAbout')}</a>
-        <Posts title={t('StayBlog')} date="22.December 2024" to="blog-updates-coming-soon-please-stay-tuned"></Posts>
-
+        <a className="cursor-default pt-[48px] text-[16px] font-normal text-[#1E1E1D] ">
+          {t("BlogAbout")}
+        </a>
+        <Posts
+          title={t("StayBlog")}
+          date="22.December 2024"
+          to="blog-updates-coming-soon-please-stay-tuned"
+        ></Posts>
 
         <div className="bg-black w-full h-[1px] mt-[32px] mb-[12px]" />
-        <div className="flex flex-row justify-end">
-          
-        </div>
+        <div className="flex flex-row justify-end"></div>
       </div>
     </div>
   );
