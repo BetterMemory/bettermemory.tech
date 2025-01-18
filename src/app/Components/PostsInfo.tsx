7c086d15048901dc0f26cc2ae8e44a091ce5c4e5
vinkonhe <vinkonhe@gmail.com>
2025-01-18 19:54:42 +0800

    feat:PostsInfo.tsx

---
+++ src/app/Components/PostsInfo.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface PostsInfoProps {
+  date: string;
+  author: string;
+}
+
+export default function PostsInfo({ date, author }: PostsInfoProps) {
+  return (
+    <div className="flex flex-col sm:items-center items-start pt-[24px]">
+      <div className="grid sm:grid-cols-[120px_1fr] gap-0.5 text-mono text-sm">
+        <div>Published</div>
+        <div>{date}</div>
+        <div className="max-sm:mt-2">Author</div>
+        <div>{author}</div>
+      </div>
+    </div>
+  );
+}
