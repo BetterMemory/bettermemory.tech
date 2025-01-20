d1001218283a75e5cc85cde1810cdc448346bd9f
vinkonhe <vinkonhe@gmail.com>
2025-01-20 23:06:40 +0800

    update:Card.tsx

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-
-
 export default function Card() {
   return (
-
+    <div className="flex flex-col mt-[24px] m-[0xp] py-[12px] px-[12px] border border-[#C0BDAD] shadow-[3px_3px_0px_#C0BDAD]"></div>
   );
 }
