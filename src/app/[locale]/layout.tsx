82609a4f10ee8058fe8ca88595200bef15f5bb5a
vinkonhe <vinkonhe@gmail.com>
2025-01-30 23:34:44 +0800

    update:umami

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,6 @@ import { notFound } from "next/navigation";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 
-
 const jetBrainsMono = localFont({
   src: "../fonts/JetBrainsMono[wght].ttf",
   weight: "100 900",
@@ -17,21 +16,15 @@ export const metadata: Metadata = {
   description: "Empowering the Next Generation of Learners.",
 };
 
-export default async function LocaleLayout(
-  props: {
-    children: React.ReactNode;
-    params: Promise<{ locale: string }>;
-  }
-) {
+export default async function LocaleLayout(props: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
   const params = await props.params;
 
-  const {
-    locale
-  } = params;
+  const { locale } = params;
 
-  const {
-    children
-  } = props;
+  const { children } = props;
 
   if (!routing.locales.includes(locale as any)) {
     notFound();
@@ -41,6 +34,13 @@ export default async function LocaleLayout(
 
   return (
     <html lang={locale}>
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="254620c4-493c-4913-bc08-898cc20107e0"
+        ></script>
+      </head>
       <body className={`${jetBrainsMono.className} antialiased bg-[#F5F4EF]`}>
         <NextIntlClientProvider messages={messages}>
           {children}
